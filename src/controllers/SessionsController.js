1a96bf81e7c8fd50e92139119107e1b24b963c0c
Vinicius Ramos <viniramos89@gmail.com>
2023-05-30 17:23:00 -0300

    Create_token

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,7 +1,32 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require ("bcryptjs");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
+
 class SessionsController {
   async create(request, response){
     const { email, password } = request.body
-    return response.json({ email, password })
+
+    const user = await knex("users").where({ email }).first(); // Verificação de usuário.
+
+    if(!user) {
+      throw new AppError("Email ou senha incorreto", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password); // verificação de senha.
+
+    if(!passwordMatched) {
+      throw new AppError("E-mail ou senha incorreto", 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({ user, token });
   }
 }
 
