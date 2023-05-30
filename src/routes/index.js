7d05436d3186963b699b24ad81cfc419201f65cc
Vinicius Ramos <viniramos89@gmail.com>
2023-05-30 16:18:34 -0300

    Create_Sessions

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const notesRouter = require("./notes.routes");
 const tagsRouter = require("./tags.routes");
+const sessionRoutes = require("./sessions.routes")
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRouter);
+routes.use("/sessions", sessionRoutes),
 
 
 module.exports = routes;
