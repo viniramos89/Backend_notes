458570c93aa52c856337504c0baa89c6c42dd129
Vinicius Ramos <viniramos89@gmail.com>
2023-05-31 16:05:46 -0300

    Create_Avatar

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 require("express-async-errors");
 
 const migrationsRun = require("./database/sqlite/migrations");
-
 const AppError = require("./utils/AppError");
-
+const uploadConfig = require("./configs/upload")
 const express = require('express');
-
-const routes = require("./routes")
+const routes = require("./routes");
 
 migrationsRun();
 
 const app = express();
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes); 
 
